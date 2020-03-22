09aba4cf1cb1bf0078ee955edb66698617a22ae2
aco915 <julius.preuschoff@haw-hamburg.de>
2020-03-22 13:50:25 +0100

    comments on led class

--- beamersteuerung/src/led.cpp
+++ beamersteuerung/src/led.cpp
@@ -1,16 +1,19 @@
 #include "led.h"
 
+// choosing the pinNumber for the LED
 led::led(int pinNumber)
 {
     this->pinNumber = pinNumber;
-    pinMode(pinNumber, OUTPUT);
+    pinMode(pinNumber, OUTPUT); // Setting the pinMode on OUTPUT
 }
 
+/* Switching the LED on */
 void led::switchLED_On()
 {
     digitalWrite(pinNumber, HIGH);
 }
 
+/* Switching the LED off */
 void led::switchLED_Off()
 {
     digitalWrite(pinNumber, LOW);
